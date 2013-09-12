87624e60ddb748e474ebc08620577fe5124e1882
Maciej Pasternacki <maciej@3ofcoins.net>
2013-09-12 17:31:23 +0200

    Refactor stuff
    - don't require chef-zero, not needed in runtime
    - use Erubis, not Ruby
    - escape HTML by default in templates
    - factor out chef_server to a method
    - move logic out of node list template, use :locals to pass parameters

--- chefapp.rb
+++ chefapp.rb
@@ -1,17 +1,26 @@
+require 'erubis'
 require 'sinatra'
 require 'ridley'
-require 'chef_zero/server'
-
-include ERB::Util
 
 class ChefApp < Sinatra::Base
+  set :erb, :escape_html => true
+
+  def chef_server
+    @chef_server ||= Ridley.new(
+      server_url: "http://127.0.0.1:4000",
+      client_name: "marta",
+      client_key: File.join(File.dirname(__FILE__), 'features/fixtures/stub.pem'))
+  end
 
   get '/' do
     erb :index
   end
 
   get '/nodes' do
-    erb :node_list
+    erb :node_list, locals: {
+      nodes: chef_server.node.all,
+      environments: chef_server.environment.all
+    }
   end
 
   get '/node' do
@@ -33,8 +42,7 @@ class ChefApp < Sinatra::Base
   #     d. handle undefined method `[]' for nil:NilClass (basically = an `if` statement?)
   #     e. extract the code to an erb file (?)
 
-    @my_server = Ridley.new(server_url: "http://127.0.0.1:4000", client_name: "marta", client_key: "/home/marta/.chef/marta.pem")
-    @nodes = @my_server.node.all
+    @nodes = chef_server.node.all
     @node_array = Array.new
     @node_name = request.path.delete "/"
     @nodes.each do |node|
@@ -42,23 +50,22 @@ class ChefApp < Sinatra::Base
     end
 
     code = %q{
-      <h3><%= @my_server.node.find(@node_name)[:name] %></h3>
-     <p><%= @my_server.node.find(@node_name)[:automatic][:fqdn] %> (<%= @my_server.node.find(@node_name)[:automatic][:ipaddress] %>)</p>
-      <p>Environment: <%= @my_server.node.find(@node_name)[:chef_environment] %></p>
-      <p>Tags:<ul class="inline"><% @my_server.node.find(@node_name)[:normal][:tags].each do |tag| %>
+      <h3><%= chef_server.node.find(@node_name)[:name] %></h3>
+     <p><%= chef_server.node.find(@node_name)[:automatic][:fqdn] %> (<%= chef_server.node.find(@node_name)[:automatic][:ipaddress] %>)</p>
+      <p>Environment: <%= chef_server.node.find(@node_name)[:chef_environment] %></p>
+      <p>Tags:<ul class="inline"><% chef_server.node.find(@node_name)[:normal][:tags].each do |tag| %>
          <li><%= tag %></li>
        <% end %>
        </ul></p>
      <h4>Run list:</h4>
-       <p><ul><% @my_server.node.find(@node_name)[:run_list].each do |run_list| %>
+       <p><ul><% chef_server.node.find(@node_name)[:run_list].each do |run_list| %>
          <li><%= run_list %></li>
        <% end %>
        </ul></p>
      <h4>Attributes (JSON)</h4>
-     <pre><%= html_escape(JSON.pretty_generate(@my_server.node.find(@node_name)._attributes_, :indent => "  ", :array_nl => "\n")) %></pre>
+     <pre><%= JSON.pretty_generate(chef_server.node.find(@node_name)._attributes_, :indent => "  ", :array_nl => "\n") %></pre>
     }
 
     erb code
-
   end
 end
