2b775a5ac16603dcd8dab348c5aeaf712f30b53b
Marta Paciorkowska <marta@paciorkowska.pl>
2013-09-12 02:49:49 +0200

    Creates a customized node detail view for each node.

--- chefapp.rb
+++ chefapp.rb
@@ -26,22 +26,37 @@ class ChefApp < Sinatra::Base
   #   take a :node_name and redirect the above URL to a dynamically created page w/the "node" layout,
   #   populated w/ details of the given node. Break it down into smaller steps:
   #     a. create a :node_name redirection & all of the URLs end in the
-  #        "uncustomized" erb :node
-  #     b. create a "customized" view in node.erb for the node that's
-  #        specified in the URL avobe.
+  #        "uncustomized" erb :node ==> done.
+  #     b. remove the redundant array w/ node names; if a wrong node name is passed,
+  #        it can be handled via an `if` statement. ==> done: array removed.
+  #     C. create a "customized" view of node details in code below. ==> done.
+  #     d. handle undefined method `[]' for nil:NilClass (basically = repairing the `if` statement)
+  #     e. extract the code to an erb file (?)
 
-    my_server = Ridley.new(server_url: "http://127.0.0.1:4000", client_name: "marta", client_key: "/home/marta/.chef/marta.pem")
-    @nodes = my_server.node.all
+    @my_server = Ridley.new(server_url: "http://127.0.0.1:4000", client_name: "marta", client_key: "/home/marta/.chef/marta.pem")
+    @nodes = @my_server.node.all
     @node_array = Array.new
     @node_name = request.path.delete "/"
     @nodes.each do |node|
       @node_array.push(node[:name])
     end
 
-    if @node_array == nil
-        "Sorry, the node you're looking for doesn't exist."
-      else
-        p my_server.node.find(@node_name)[:name] + my_server.node.find(@node_name)[:automatic][:fqdn] + my_server.node.find(@node_name)[:name] + my_server.node.find(@node_name)[:automatic][:ipaddress]
-    end
+    code = '<h3><%= @my_server.node.find(@node_name)[:name] %></h3>
+    <p><%= @my_server.node.find(@node_name)[:name] %>(<%= @my_server.node.find(@node_name)[:automatic][:fqdn] %>)</p>
+    <p><%= @my_server.node.find(@node_name)[:name] %>[<%= @my_server.node.find(@node_name)[:automatic][:ipaddress] %>]</p>
+    <p>Environment: <%= @my_server.node.find(@node_name)[:chef_environment] %></p>
+    <p>Tags:<ul class="inline"><% @my_server.node.find(@node_name)[:normal][:tags].each do |tag| %>
+        <li><%= tag %></li>
+      <% end %>
+      </ul></p>
+    <h4>Run list:</h4>
+      <p><ul><% @my_server.node.find(@node_name)[:run_list].each do |run_list| %>
+        <li><%= run_list %></li>
+      <% end %>
+      </ul></p>
+    <h4>Attributes (JSON)</h4>
+    <%= escape_html(@my_server.node.find(@node_name)._attributes_) %>'
+    erb code
+
   end
 end
