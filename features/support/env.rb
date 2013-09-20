263543e97394eb9199dc660bb7292aa0e56bdb10
Maciej Pasternacki <maciej@3ofcoins.net>
2013-09-20 17:04:49 +0200

    Use Capybara for easier navigation

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,18 +1,10 @@
-require 'rack/test'
-require 'wrong'
-
 _lib = File.realpath(File.join(File.dirname(__FILE__), '../../lib'))
 $:.unshift(_lib) unless $:.include?(_lib)
 
 require 'chef-browser'
 
-# The `app` method is needed by rack-test
-module ChefBrowser
-  module SpecHelper
-    def app
-      App
-    end
-  end
-end
+require 'capybara/cucumber'
+Capybara.app = ChefBrowser::App
 
-World(Rack::Test::Methods, Wrong, ChefBrowser::SpecHelper)
+require 'wrong'
+World(Wrong)
