99be8e2bfbabeda5a4cd851f24b0489b68461274
Marta Paciorkowska <marta@paciorkowska.pl>
2013-11-05 14:38:57 +0100

    Sorts node search results properly.

--- lib/chef-browser/app.rb
+++ lib/chef-browser/app.rb
@@ -83,7 +83,7 @@ module ChefBrowser
           search_query: search_query
         }
       else
-        search_results = chef_server.search(:node, search_query, :sort => 'name ASC')
+        search_results = chef_server.search(:node, search_query).sort_by {|k| k[:name]}
         erb :node_search, locals: {
           search_query: search_query,
           search_results: search_results
