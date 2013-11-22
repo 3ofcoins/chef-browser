37541a23359edfa5b110e800491207937f515e31
Maciej Pasternacki <maciej@3ofcoins.net>
2013-11-22 23:04:45 +0100

    Add optional HTML validation (use cucumber profile: cucumber -p validate). It calls out to an external validator.nu service, so it's not on by default.

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,34 @@ require 'chef-browser'
 
 require 'capybara/cucumber'
 require 'capybara/webkit'
-Capybara.app = ChefBrowser::App
+
+require 'net/http'
+
+# Raise exception if `html_str` is not valid HTML according to
+# http://html5.validator.nu/
+def validate_html(html_str)
+  resp = Net::HTTP.start('html5.validator.nu') do |http|
+    http.post '/?out=text', html_str, { 'Content-Type' => 'text/html; charset=utf-8' }
+  end
+  resp.value                    # raise error if not 2xx
+  unless resp.body =~ /^The document is valid HTML5/
+    lines = []
+    html_str.lines.each_with_index do |line, i|
+      lines << "#{i+1}\t#{line}"
+    end
+    raise "Invalid HTML:\n\n#{lines.join}\n\n#{resp.body}"
+  end
+end
+
+if ENV['VALIDATE_HTML']
+  Capybara.app = lambda do |env|
+    resp = ChefBrowser::App.call(env)
+    validate_html(resp[2].join) if resp[1]['Content-Type'] =~ /^text\/html\s*(;.*)?$/
+    resp
+  end
+else
+  Capybara.app = ChefBrowser::App
+end
 Capybara.javascript_driver = :webkit
 
 require 'wrong'
