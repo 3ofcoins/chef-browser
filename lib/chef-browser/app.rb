f0b32b641a5ec3bcc2093bfbc8a190f821f145c6
Maciej Pasternacki <maciej@3ofcoins.net>
2013-12-17 15:00:39 +0100

    Make fuzzy query when sending it to the Chef server, to have user see the query they typed rather than a processed one.

--- lib/chef-browser/app.rb
+++ lib/chef-browser/app.rb
@@ -92,11 +92,13 @@ module ChefBrowser
     def resource_list(resource, data_bag=nil)
       if search_query && resource != :data_bag
         @title << search_query
+        raw_query = search_query
+        raw_query = "tags:*#{raw_query}* OR roles:*#{raw_query}* OR fqdn:*#{raw_query}* OR addresses:*#{raw_query}*" unless raw_query[':']
         if data_bag
           # For data bag search, Ridley returns untyped Hashie::Mash, we want to augment it with our methods.
-          resources = chef_server.search(data_bag.chef_id, search_query).map { |attrs| Ridley::DataBagItemObject.new(nil, data_bag, attrs[:raw_data]) }
+          resources = chef_server.search(data_bag.chef_id, raw_query).map { |attrs| Ridley::DataBagItemObject.new(nil, data_bag, attrs[:raw_data]) }
         else
-          resources = chef_server.search(resource, search_query)
+          resources = chef_server.search(resource, raw_query)
         end
       elsif data_bag
         resources = data_bag.item.all
@@ -107,10 +109,7 @@ module ChefBrowser
     end
 
     def search_query
-      if @search_query ||= params['q']
-        @search_query = "tags:*#{@search_query}* OR roles:*#{@search_query}* OR fqdn:*#{@search_query}* OR addresses:*#{@search_query}*" unless @search_query[":"]
-        @search_query
-      end
+      @search_query || params['q']
     end
 
     ##
