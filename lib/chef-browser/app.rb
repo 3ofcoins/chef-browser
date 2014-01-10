33c6afa0b6486ccd463673d4c1d0b6aef339244f
Marta Paciorkowska <marta@paciorkowska.pl>
2014-01-10 12:57:24 +0100

    Block instead of special connection object

--- lib/chef-browser/app.rb
+++ lib/chef-browser/app.rb
@@ -271,19 +271,16 @@ module ChefBrowser
       pass unless data_bag_item
       @title << params[:data_bag_item_id]
       if params["key"] != "" || params["upload"] != nil
-        begin
-          @encrypted_data_bag_secret = (params["key"] unless params["key"] == "") || File.open(params["upload"][:tempfile]).read
-          chef_server = ::Ridley.new({
-                            server_url: settings.rb.server_url,
-                            client_name: settings.rb.client_name,
-                            client_key: settings.rb.client_key,
-                            encrypted_data_bag_secret: @encrypted_data_bag_secret
-                          }.merge(settings.rb.connection))
-          data_bag_item = chef_server.data_bag.find(params[:data_bag_id]).item.find(params[:data_bag_item_id])
-          data_bag_item.decrypt
-          erb :data_bag_item, locals: { data_bag_item: data_bag_item, decrypted: true } 
-        ensure
-          @encrypted_data_bag_secret = nil
+        encrypted_data_bag_secret = (params["key"] unless params["key"] == "") || File.open(params["upload"][:tempfile]).read
+        ::Ridley.open({
+            server_url: settings.rb.server_url,
+            client_name: settings.rb.client_name,
+            client_key: settings.rb.client_key,
+            encrypted_data_bag_secret: encrypted_data_bag_secret
+            }.merge(settings.rb.connection)) do |server|
+              data_bag_item = server.data_bag.find(params[:data_bag_id]).item.find(params[:data_bag_item_id])
+              data_bag_item.decrypt
+              erb :data_bag_item, locals: { data_bag_item: data_bag_item, decrypted: true }
         end
       else
         erb :data_bag_item, locals: { data_bag_item: data_bag_item, decrypted: false }
