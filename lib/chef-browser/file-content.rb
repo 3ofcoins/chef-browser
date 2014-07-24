a2136fa3716a32e3787745239ea60cc79849292d
Marta Paciorkowska <marta@paciorkowska.pl>
2014-07-24 15:03:39 +0200

    Inform when a file cannot be displayed, add download button to every file

--- lib/chef-browser/file-content.rb
+++ lib/chef-browser/file-content.rb
@@ -13,25 +13,29 @@ module ChefBrowser
     end
 
     class << self
-      def show(file, extname, content)
+      def show_file(file, extname, content)
         inside = FileContent.new(file.name, file.url, content)
         if inside.image?
           "<img src = '#{inside.path}'>"
         elsif inside.text?
           FileContent.highlight_file(inside.name, extname, inside.data)
-        else
-          # download
         end
       end
 
       def highlight_file(filename, extname, content)
         lexer = (Linguist::Language[extname.gsub(/^\./, '')] ||
-                 Linguist::Language.find_by_filename(filename).first)
+                 Linguist::Language.find_by_filename(filename).first ||
+                 Linguist::Language[Linguist.interpreter_from_shebang(content)]
+                )
         if lexer.nil?
-          "<pre>#{content}</pre>"
+          Pygments.highlight(content, lexer: 'text', options: { encoding: 'utf-8', formatter: 'html', linenos: 'inline' })
         else
           lexer.colorize(content, options: { encoding: 'utf-8', formatter: 'html', linenos: 'inline' })
         end
+      end
+
+      def download_file(url)
+        "<a role='button' class='btn btn-default btn-primary' href='#{url}'>Download file</a>"
       end
     end
   end
